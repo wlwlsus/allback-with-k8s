66ef9d26700fa85fc4ea57fe902f097b482a22de
성원준 <cadqe13@gmail.com>
2023-04-17 16:27:10 +0900

    Add : 관리자 프로젝트 추가

---
+++ cygi-admin/src/test/java/com/allback/cygiadmin/CygiAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allback.cygiadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CygiAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
