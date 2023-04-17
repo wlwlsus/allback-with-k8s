96cf6402bef6a173a99a2b5faffd0e1de581cc49
성원준 <cadqe13@gmail.com>
2023-04-17 16:24:07 +0900

    Add : 유저 프로젝트 추가

---
+++ cygi-user/src/main/java/com/allback/cygiuser/CygiUserApplication.java
@@ -0,0 +1,13 @@
+package com.allback.cygiuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CygiUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CygiUserApplication.class, args);
+	}
+
+}
