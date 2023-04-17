4bdd333bffc446c9ce2fce5853f818aa11577404
성원준 <cadqe13@gmail.com>
2023-04-17 16:29:30 +0900

    Add : 공연 프로젝트 추가

---
+++ cygi-concert/src/main/java/com/allback/cygiconcert/CygiConcertApplication.java
@@ -0,0 +1,13 @@
+package com.allback.cygiconcert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CygiConcertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CygiConcertApplication.class, args);
+	}
+
+}
---
+++ cygi-concert/src/test/java/com/allback/cygiconcert/CygiConcertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allback.cygiconcert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CygiConcertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
