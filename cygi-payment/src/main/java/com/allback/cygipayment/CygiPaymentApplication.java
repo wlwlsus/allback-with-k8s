5a215f150645d1237ae24e03a56f6864ed4eb051
성원준 <cadqe13@gmail.com>
2023-04-17 16:32:53 +0900

    Add : 결제 프로젝트 추가

---
+++ cygi-payment/src/main/java/com/allback/cygipayment/CygiPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.allback.cygipayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CygiPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CygiPaymentApplication.class, args);
+	}
+
+}
