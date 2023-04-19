d483684007efc537cdc7132a809de0d1c11a778f
kjskjs356 <kjskjs356@gmail.com>
2023-04-19 17:54:13 +0900

    fix: 최초 접속시 비로그인 상태로 수정

--- cygi-front/src/pages/Main.jsx
+++ cygi-front/src/pages/Main.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 export default function Main() {
   const navigate = useNavigate();
-  const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(false);
 
   // 로그인 여부에 따라 메인 페이지 다르게 표시
   // 추후 토큰 인증으로 교체
