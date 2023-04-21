5bf4fecf518660207b5e06c7abb4fa9364e7cf78
swj <159632_@naver.com>
2023-04-21 20:57:41 +0900

    fix: 컨버터 이슈 해결

--- cygi-concert/src/main/java/com/allback/cygiconcert/service/ConcertService.java
+++ cygi-concert/src/main/java/com/allback/cygiconcert/service/ConcertService.java
@@ -2,13 +2,14 @@ package com.allback.cygiconcert.service;
 
 import com.allback.cygiconcert.dto.request.ConcertReqDto;
 import com.allback.cygiconcert.dto.response.ConcertResDto;
-import org.springframework.data.domain.Page;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface ConcertService {
 
     void registConcert(ConcertReqDto concertReqDto, MultipartFile image) throws Exception;
-    Page<ConcertResDto.CustomConcertResDto> getConcertPage(int page);
+    List<ConcertResDto> getConcertPage(int page);
 
-    ConcertResDto.CustomConcertResDto getConcert(Long concertId);
+    ConcertResDto getConcert(Long concertId);
 }
