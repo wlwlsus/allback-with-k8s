76f4e7a10fb6c0390bd2d63cb5556b8b7c47a4d1
kjskjs356 <kjskjs356@gmail.com>
2023-05-02 17:47:06 +0900

    fix: 헤더 변경 & 무한 스크롤 작업중

--- cygi-front/src/components/login-main/ConcertList.jsx
+++ cygi-front/src/components/login-main/ConcertList.jsx
@@ -1,20 +1,23 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import style from "./ConcertList.module.css";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { $_concert } from "util/axios";
+import InfiniteScroll from "react-infinite-scroller";
 
 export default function ConcertList() {
   const navigate = useNavigate();
-  const bottomObserver = useRef(null);
   let nowTime = new Date();
 
-  const [page, setPage] = useState(1);
-  const [bottom, setBottom] = useState(null);
-  const [list, setList] = useState([]);
-
-  const { isLoading, data, refetch } = useQuery(["concert"], () =>
-    $_concert.get(`/concert?page=${page}`)
+  const { data, fetchNextPage, hasNextPage, isLoading } = useInfiniteQuery(
+    ["concert"],
+    ({ pageParam = 1 }) => $_concert.get(`/concert?page=${pageParam}`),
+    {
+      getNextPageParam: (lastPage) => {
+        console.log("다음데이터");
+        return lastPage.next || undefined;
+      },
+    }
   );
 
   const onSelect = (id) => {
@@ -25,35 +28,6 @@ export default function ConcertList() {
     });
   };
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        if (entries[0].isIntersecting) {
-          setPage(page + 1);
-          refetch();
-        }
-      },
-      { threshold: 0.25, rootMargin: "80px" }
-    );
-    bottomObserver.current = observer;
-  }, []);
-
-  useEffect(() => {
-    const observer = bottomObserver.current;
-    if (bottom) {
-      observer.observe(bottom);
-    }
-    return () => {
-      if (bottom) {
-        observer.unobserve(bottom);
-      }
-    };
-  }, [bottom]);
-
-  useEffect(() => {
-    if (!isLoading) setList((list) => [...list, data.data]);
-  }, [isLoading]);
-
   return (
     <div className={style.total}>
       <div className={style.header}>공연 목록</div>
@@ -63,58 +37,73 @@ export default function ConcertList() {
         결제는 CAN YOU GET IT 포인트 또는 카카오 페이를 통해 이루어집니다.
       </div>
       <div className={style.container}>
-        {!isLoading &&
-          data &&
-          data.data.map((content) => {
-            let date =
-              content.endDate.slice(0, 4) +
-              "/" +
-              content.endDate.slice(5, 7) +
-              "/" +
-              content.endDate.slice(8, 10) +
-              " " +
-              content.endDate.slice(11, 13) +
-              "시" +
-              content.endDate.slice(14, 16) +
-              "분";
-            return (
-              <div
-                key={content.concertId}
-                className={
-                  content.rest === 0 || nowTime >= new Date(content.endDate)
-                    ? style.card_disable
-                    : style.card
-                }
-                onClick={() => {
-                  if (content.rest !== 0 && nowTime < new Date(content.endDate))
-                    onSelect(content.concertId);
-                }}
-              >
-                <div className={style.poster_url}>
-                  <img src={content.image} alt="" />
-                </div>
-                <div className={style.content_title}>{content.title}</div>
-                <div className={style.contents}>
-                  <div className={style.name}>
-                    <div className={style.title}>잔여좌석</div>
-                    <div className={style.content}>
-                      {content.rest === 0
-                        ? "마감"
-                        : content.rest + "/" + content.all}
+        <InfiniteScroll
+          hasMore={hasNextPage}
+          loadMore={fetchNextPage}
+          loader={
+            <div className="loader" key={0}>
+              Loading ...
+            </div>
+          }
+          className={style.infinity}
+        >
+          {!isLoading &&
+            data.pages.map((page) => {
+              console.log(data);
+              return page.data.map((content) => {
+                let date =
+                  content.endDate.slice(0, 4) +
+                  "/" +
+                  content.endDate.slice(5, 7) +
+                  "/" +
+                  content.endDate.slice(8, 10) +
+                  " " +
+                  content.endDate.slice(11, 13) +
+                  "시" +
+                  content.endDate.slice(14, 16) +
+                  "분";
+                return (
+                  <div
+                    key={content.concertId}
+                    className={
+                      content.rest === 0 || nowTime >= new Date(content.endDate)
+                        ? style.card_disable
+                        : style.card
+                    }
+                    onClick={() => {
+                      if (
+                        content.rest !== 0 &&
+                        nowTime < new Date(content.endDate)
+                      )
+                        onSelect(content.concertId);
+                    }}
+                  >
+                    <div className={style.poster_url}>
+                      <img src={content.image} alt="" />
+                    </div>
+                    <div className={style.content_title}>{content.title}</div>
+                    <div className={style.contents}>
+                      <div className={style.name}>
+                        <div className={style.title}>잔여좌석</div>
+                        <div className={style.content}>
+                          {content.rest === 0
+                            ? "마감"
+                            : content.rest + "/" + content.all}
+                        </div>
+                      </div>
+                      <div className={style.seat}>
+                        <div className={style.title}>예매마감일</div>
+                        <div className={style.content}>
+                          {nowTime >= new Date(content.endDate) ? "마감" : date}
+                        </div>
+                      </div>
                     </div>
                   </div>
-                  <div className={style.seat}>
-                    <div className={style.title}>예매마감일</div>
-                    <div className={style.content}>
-                      {nowTime >= new Date(content.endDate) ? "마감" : date}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
+                );
+              });
+            })}
+        </InfiniteScroll>
       </div>
-      <div ref={setBottom}></div>
     </div>
   );
 }
--- cygi-front/src/util/store.js
+++ cygi-front/src/util/store.js
@@ -1,6 +1,28 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 export const user = atom({
   key: "userId",
   default: "",
+  effects_UNSTABLE: [persistAtom],
+});
+
+// 공연 목록 저장하는 상태 변수
+export const concertState = atom({
+  key: "concertState",
+  default: [],
+});
+
+export const concertLastIdxState = selector({
+  key: "concertLastIdxState",
+  get: ({ get }) => {
+    const concerts = get(concertState);
+    if (concerts.length === 0) return -1;
+    return concerts[concerts.length - 1].idx;
+  },
+  set: ({ set }) => {
+    set(concertState, []);
+  },
 });
