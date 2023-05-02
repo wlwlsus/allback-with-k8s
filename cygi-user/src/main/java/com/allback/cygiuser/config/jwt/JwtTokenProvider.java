4df7ecf976ecc883f0c205cf9277ab96fb8e45ea
swj <159632_@naver.com>
2023-05-03 01:17:33 +0900

    fix : 권한 설정

--- cygi-user/src/main/java/com/allback/cygiuser/config/jwt/JwtTokenProvider.java
+++ cygi-user/src/main/java/com/allback/cygiuser/config/jwt/JwtTokenProvider.java
@@ -26,97 +26,101 @@ import java.util.stream.Collectors;
 @Component
 public class JwtTokenProvider {
 
-  private static final String AUTHORITIES_KEY = "auth";
-  private static final String BEARER_TYPE = "Bearer";
-  private static final long ACCESS_TOKEN_EXPIRE_TIME = 24 * 60 * 60 * 1000L;          // 1일
-  private static final long REFRESH_TOKEN_EXPIRE_TIME = 90 * 24 * 60 * 60 * 1000L;    // 90일
-  private static final int REFRESH_TOKEN_EXPIRE_TIME_COOKIE = Integer.MAX_VALUE;
+	private static final String AUTHORITIES_KEY = "auth";
+	private static final String NICKNAME_KEY = "nickname";
+	private static final String EMAIL_KEY = "email";
+	private static final String BEARER_TYPE = "Bearer";
+	private static final long ACCESS_TOKEN_EXPIRE_TIME = 30 * 24 * 60 * 60 * 1000L;          // 30일
+	private static final long REFRESH_TOKEN_EXPIRE_TIME = 90 * 24 * 60 * 60 * 1000L;    // 90일
+	private static final int REFRESH_TOKEN_EXPIRE_TIME_COOKIE = Integer.MAX_VALUE;
 
-  private final Key key;
+	private final Key key;
 
-  public JwtTokenProvider(@Value("${jwt.secret}") String secretKey) {
-    byte[] keyBytes = Decoders.BASE64.decode(secretKey);
-    this.key = Keys.hmacShaKeyFor(keyBytes);
-  }
+	public JwtTokenProvider(@Value("${jwt.secret}") String secretKey) {
+		byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+		this.key = Keys.hmacShaKeyFor(keyBytes);
+	}
 
-  public UserTestResDto.TokenInfo generateToken(String id, String role) {
-    long now = (new Date()).getTime();
-    // Access Token 생성
-    String accessToken = Jwts.builder()
-        .setSubject(id)
-        .claim(AUTHORITIES_KEY, role)
-        .signWith(key, SignatureAlgorithm.HS256)
-        .setExpiration(new Date(now + ACCESS_TOKEN_EXPIRE_TIME))
-        .compact();
+	public UserTestResDto.TokenInfo generateToken(String uuid, String nickname, String email, String role) {
+		long now = (new Date()).getTime();
+		// Access Token 생성
+		String accessToken = Jwts.builder()
+				.setSubject(uuid)
+				.claim(NICKNAME_KEY, nickname)
+				.claim(EMAIL_KEY, email)
+				.claim(AUTHORITIES_KEY, role)
+				.signWith(key, SignatureAlgorithm.HS256)
+				.setExpiration(new Date(now + ACCESS_TOKEN_EXPIRE_TIME))
+				.compact();
 
-    // Refresh Token 생성
-    String refreshToken = Jwts.builder()
-        .setExpiration(new Date(now + REFRESH_TOKEN_EXPIRE_TIME))
-        .signWith(key, SignatureAlgorithm.HS256)
-        .compact();
+		// Refresh Token 생성
+		String refreshToken = Jwts.builder()
+				.setExpiration(new Date(now + REFRESH_TOKEN_EXPIRE_TIME))
+				.signWith(key, SignatureAlgorithm.HS256)
+				.compact();
 
-    return UserTestResDto.TokenInfo.builder()
-        .grantType(BEARER_TYPE)
-        .accessToken(accessToken)
-        .refreshToken(refreshToken)
-        .refreshTokenExpirationTime(REFRESH_TOKEN_EXPIRE_TIME)
-        .build();
-  }
+		return UserTestResDto.TokenInfo.builder()
+				.grantType(BEARER_TYPE)
+				.accessToken(accessToken)
+				.refreshToken(refreshToken)
+				.refreshTokenExpirationTime(REFRESH_TOKEN_EXPIRE_TIME)
+				.build();
+	}
 
-  // JWT 토큰을 복호화하여 토큰에 들어있는 정보를 꺼내는 메서드
-  public Authentication getAuthentication(String accessToken) {
-    // 토큰 복호화
-    Claims claims = parseClaims(accessToken);
+	// JWT 토큰을 복호화하여 토큰에 들어있는 정보를 꺼내는 메서드
+	public Authentication getAuthentication(String accessToken) {
+		// 토큰 복호화
+		Claims claims = parseClaims(accessToken);
 
-    if (claims.get(AUTHORITIES_KEY) == null) {
-      throw new RuntimeException("권한 정보가 없는 토큰입니다.");
-    }
+		if (claims.get(AUTHORITIES_KEY) == null) {
+			throw new RuntimeException("권한 정보가 없는 토큰입니다.");
+		}
 
-    // 클레임에서 권한 정보 가져오기
-    Collection<? extends GrantedAuthority> authorities =
-        Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
-            .map(SimpleGrantedAuthority::new)
-            .collect(Collectors.toList());
+		// 클레임에서 권한 정보 가져오기
+		Collection<? extends GrantedAuthority> authorities =
+				Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+						.map(SimpleGrantedAuthority::new)
+						.collect(Collectors.toList());
 
-    // UserDetails 객체를 만들어서 Authentication 리턴
-    UserDetails principal = new User(claims.getSubject(), "", authorities);
-    return new UsernamePasswordAuthenticationToken(principal, "", authorities);
-  }
+		// UserDetails 객체를 만들어서 Authentication 리턴
+		UserDetails principal = new User(claims.getSubject(), "", authorities);
+		return new UsernamePasswordAuthenticationToken(principal, "", authorities);
+	}
 
-  // 토큰 정보를 검증하는 메서드
-  public boolean validateToken(String token) {
-    try {
-      Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
-      return true;
-    } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
-      log.info("Invalid JWT Token", e);
-    } catch (ExpiredJwtException e) {
-      log.info("Expired JWT Token", e);
-    } catch (UnsupportedJwtException e) {
-      log.info("Unsupported JWT Token", e);
-    } catch (IllegalArgumentException e) {
-      log.info("JWT claims string is empty.", e);
-    }
-    return false;
-  }
+	// 토큰 정보를 검증하는 메서드
+	public boolean validateToken(String token) {
+		try {
+			Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+			return true;
+		} catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+			log.info("Invalid JWT Token", e);
+		} catch (ExpiredJwtException e) {
+			log.info("Expired JWT Token", e);
+		} catch (UnsupportedJwtException e) {
+			log.info("Unsupported JWT Token", e);
+		} catch (IllegalArgumentException e) {
+			log.info("JWT claims string is empty.", e);
+		}
+		return false;
+	}
 
-  private Claims parseClaims(String accessToken) {
-    try {
-      return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken).getBody();
-    } catch (ExpiredJwtException e) {
-      return e.getClaims();
-    }
-  }
+	private Claims parseClaims(String accessToken) {
+		try {
+			return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken).getBody();
+		} catch (ExpiredJwtException e) {
+			return e.getClaims();
+		}
+	}
 
-  public Long getExpiration(String accessToken) {
-    // accessToken 남은 유효시간
-    Date expiration = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken).getBody().getExpiration();
-    // 현재 시간
-    long now = new Date().getTime();
-    return (expiration.getTime() - now);
-  }
+	public Long getExpiration(String accessToken) {
+		// accessToken 남은 유효시간
+		Date expiration = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken).getBody().getExpiration();
+		// 현재 시간
+		long now = new Date().getTime();
+		return (expiration.getTime() - now);
+	}
 
-  public static int getRefreshTokenExpireTimeCookie() {
-    return REFRESH_TOKEN_EXPIRE_TIME_COOKIE;
-  }
+	public static int getRefreshTokenExpireTimeCookie() {
+		return REFRESH_TOKEN_EXPIRE_TIME_COOKIE;
+	}
 }
--- cygi-user/src/main/java/com/allback/cygiuser/config/oauth/handler/OAuth2AuthenticationSuccessHandler.java
+++ cygi-user/src/main/java/com/allback/cygiuser/config/oauth/handler/OAuth2AuthenticationSuccessHandler.java
@@ -38,84 +38,84 @@ import static com.allback.cygiuser.config.oauth.repository.OAuth2AuthorizationRe
 @RequiredArgsConstructor
 public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
-  @Value("${app.oauth2.authorizedRedirectUris}")
-  private String redirectUri;
-  private final OAuth2AuthorizationRequestBasedOnCookieRepository authorizationRequestRepository;
+	@Value("${app.oauth2.authorizedRedirectUris}")
+	private String redirectUri;
+	private final OAuth2AuthorizationRequestBasedOnCookieRepository authorizationRequestRepository;
 
-  private final JwtTokenProvider jwtTokenProvider;
+	private final JwtTokenProvider jwtTokenProvider;
 
-  private final RedisTemplate<String, String> redisTemplate;
+	private final RedisTemplate<String, String> redisTemplate;
 
-  @Override
-  public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
-    String targetUrl = determineTargetUrl(request, response, authentication);
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+		String targetUrl = determineTargetUrl(request, response, authentication);
 
-    if (response.isCommitted()) {
-      log.error("Response has already been committed. Unable to redirect to " + targetUrl);
-      return;
-    }
+		if (response.isCommitted()) {
+			log.error("Response has already been committed. Unable to redirect to " + targetUrl);
+			return;
+		}
 
-    clearAuthenticationAttributes(request, response);
-    getRedirectStrategy().sendRedirect(request, response, targetUrl);
-  }
+		clearAuthenticationAttributes(request, response);
+		getRedirectStrategy().sendRedirect(request, response, targetUrl);
+	}
 
-  protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-    Optional<String> redirectUri = CookieUtil.getCookie(request, OAuth2AuthorizationRequestBasedOnCookieRepository.REDIRECT_URI_PARAM_COOKIE_NAME)
-        .map(Cookie::getValue);
+	protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+		Optional<String> redirectUri = CookieUtil.getCookie(request, OAuth2AuthorizationRequestBasedOnCookieRepository.REDIRECT_URI_PARAM_COOKIE_NAME)
+				.map(Cookie::getValue);
 
-    if (redirectUri.isPresent() && !isAuthorizedRedirectUri(redirectUri.get())) {
-      log.error("determineTargetUrl - redirectUri : {} , 인증을 진행할 수 없습니다.", redirectUri);
-      throw new IllegalArgumentException("Sorry! We've got an Unauthorized Redirect URI and can't proceed with the authentication");
-    }
+		if (redirectUri.isPresent() && !isAuthorizedRedirectUri(redirectUri.get())) {
+			log.error("determineTargetUrl - redirectUri : {} , 인증을 진행할 수 없습니다.", redirectUri);
+			throw new IllegalArgumentException("Sorry! We've got an Unauthorized Redirect URI and can't proceed with the authentication");
+		}
 
-    String targetUrl = redirectUri.orElse(getDefaultTargetUrl());
+		String targetUrl = redirectUri.orElse(getDefaultTargetUrl());
 
-    OAuth2AuthenticationToken authToken = (OAuth2AuthenticationToken) authentication;
-    ProviderType providerType = ProviderType.valueOf(authToken.getAuthorizedClientRegistrationId().toUpperCase());
-    OidcUser user = ((OidcUser) authentication.getPrincipal());
-    OAuth2UserInfo userInfo = OAuth2UserInfoFactory.getOAuth2UserInfo(providerType, user.getAttributes());
-    Collection<? extends GrantedAuthority> authorities = ((OidcUser) authentication.getPrincipal()).getAuthorities();
+		OAuth2AuthenticationToken authToken = (OAuth2AuthenticationToken) authentication;
+		ProviderType providerType = ProviderType.valueOf(authToken.getAuthorizedClientRegistrationId().toUpperCase());
+		OidcUser user = ((OidcUser) authentication.getPrincipal());
+		OAuth2UserInfo userInfo = OAuth2UserInfoFactory.getOAuth2UserInfo(providerType, user.getAttributes());
+		Collection<? extends GrantedAuthority> authorities = ((OidcUser) authentication.getPrincipal()).getAuthorities();
 
-    RoleType roleType = hasAuthority(authorities, RoleType.ROLE_ADMIN.name()) ? RoleType.ROLE_ADMIN : RoleType.ROLE_USER;
+		RoleType roleType = hasAuthority(authorities, RoleType.ROLE_ADMIN.name()) ? RoleType.ROLE_ADMIN : RoleType.ROLE_USER;
+		UserTestResDto.TokenInfo tokenInfo = jwtTokenProvider.generateToken(userInfo.getId(),
+				userInfo.getName(), userInfo.getEmail(), roleType.name());
 
-    UserTestResDto.TokenInfo tokenInfo = jwtTokenProvider.generateToken(userInfo.getId(), roleType.name());
+		redisTemplate.opsForValue()
+				.set("RT:" + authentication.getName(), tokenInfo.getRefreshToken(), tokenInfo.getRefreshTokenExpirationTime(), TimeUnit.MILLISECONDS);
 
-    redisTemplate.opsForValue()
-        .set("RT:" + authentication.getName(), tokenInfo.getRefreshToken(), tokenInfo.getRefreshTokenExpirationTime(), TimeUnit.MILLISECONDS);
+		CookieUtil.deleteCookie(request, response, REFRESH_TOKEN);
+		CookieUtil.addCookie(response, REFRESH_TOKEN, tokenInfo.getRefreshToken(), getRefreshTokenExpireTimeCookie());
 
-    CookieUtil.deleteCookie(request, response, REFRESH_TOKEN);
-    CookieUtil.addCookie(response, REFRESH_TOKEN, tokenInfo.getRefreshToken(), getRefreshTokenExpireTimeCookie());
+		return UriComponentsBuilder.fromUriString(targetUrl)
+				.queryParam("accessToken", tokenInfo.getAccessToken())
+				.queryParam("refreshToken", tokenInfo.getRefreshToken())
+				.build().toUriString();
+	}
 
-    return UriComponentsBuilder.fromUriString(targetUrl)
-        .queryParam("accessToken", tokenInfo.getAccessToken())
-        .queryParam("refreshToken", tokenInfo.getRefreshToken())
-        .build().toUriString();
-  }
+	protected void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
+		super.clearAuthenticationAttributes(request);
+		authorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
+	}
 
-  protected void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
-    super.clearAuthenticationAttributes(request);
-    authorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
-  }
+	private boolean hasAuthority(Collection<? extends GrantedAuthority> authorities, String authority) {
+		if (authorities == null) {
+			return false;
+		}
 
-  private boolean hasAuthority(Collection<? extends GrantedAuthority> authorities, String authority) {
-    if (authorities == null) {
-      return false;
-    }
+		for (GrantedAuthority grantedAuthority : authorities) {
+			if (authority.equals(grantedAuthority.getAuthority())) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    for (GrantedAuthority grantedAuthority : authorities) {
-      if (authority.equals(grantedAuthority.getAuthority())) {
-        return true;
-      }
-    }
-    return false;
-  }
+	private boolean isAuthorizedRedirectUri(String uri) {
+		URI clientRedirectUri = URI.create(uri);
+		URI authorizedUri = URI.create(redirectUri);
 
-  private boolean isAuthorizedRedirectUri(String uri) {
-    URI clientRedirectUri = URI.create(uri);
-    URI authorizedUri = URI.create(redirectUri);
-
-    return authorizedUri.getHost().equalsIgnoreCase(clientRedirectUri.getHost())
-        && authorizedUri.getPort() == clientRedirectUri.getPort();
-  }
+		return authorizedUri.getHost().equalsIgnoreCase(clientRedirectUri.getHost())
+				&& authorizedUri.getPort() == clientRedirectUri.getPort();
+	}
 
 }
