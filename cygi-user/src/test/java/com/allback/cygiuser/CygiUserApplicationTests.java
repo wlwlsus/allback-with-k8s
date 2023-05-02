ef93b2f29a6c8a374c3fe1e03e22748229fdfcc3
김동연 <rlaehddus815@gmail.com>
2023-05-02 15:44:42 +0900

    Update cygi-user/src/test/java/com/allback/cygiuser/CygiUserApplicationTests.java

--- cygi-user/src/test/java/com/allback/cygiuser/CygiUserApplicationTests.java
+++ cygi-user/src/test/java/com/allback/cygiuser/CygiUserApplicationTests.java
@@ -3,7 +3,7 @@ package com.allback.cygiuser;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class CygiUserApplicationTests {
 
 	@Test
