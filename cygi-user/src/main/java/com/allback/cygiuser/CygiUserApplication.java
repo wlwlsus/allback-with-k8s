326cc21d19861b75d847e6c4d241c001cbb3439b
성원준 <cadqe13@gmail.com>
2023-05-04 09:05:54 +0900

    fix: 유레카 임시 종료

--- cygi-user/src/main/java/com/allback/cygiuser/CygiUserApplication.java
+++ cygi-user/src/main/java/com/allback/cygiuser/CygiUserApplication.java
@@ -2,12 +2,10 @@ package com.allback.cygiuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class CygiUserApplication {
 
