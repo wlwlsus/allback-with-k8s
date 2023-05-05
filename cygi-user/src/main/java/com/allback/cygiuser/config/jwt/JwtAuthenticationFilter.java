00c051b5661d7959df71b1751c49fb6522a965f9
swj <159632_@naver.com>
2023-05-05 15:24:02 +0900

    fix: 권한 쪽 수정

--- cygi-user/src/main/java/com/allback/cygiuser/config/jwt/JwtAuthenticationFilter.java
+++ cygi-user/src/main/java/com/allback/cygiuser/config/jwt/JwtAuthenticationFilter.java
@@ -30,18 +30,18 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		// 1. Request Header 에서 JWT 토큰 추출
-		String token = resolveToken((HttpServletRequest) request);
-
-		// 2. validateToken 으로 토큰 유효성 검사
-		if (token != null && jwtTokenProvider.validateToken(token)) {
-			// (추가) Redis 에 해당 accessToken logout 여부 확인
-			String isLogout = redisTemplate.opsForValue().get(token);
-			if (ObjectUtils.isEmpty(isLogout)) {
-				// 토큰이 유효할 경우 토큰에서 Authentication 객체를 가지고 와서 SecurityContext 에 저장
-				Authentication authentication = jwtTokenProvider.getAuthentication(token);
-				SecurityContextHolder.getContext().setAuthentication(authentication);
-			}
-		}
+//		String token = resolveToken((HttpServletRequest) request);
+//
+//		// 2. validateToken 으로 토큰 유효성 검사
+//		if (token != null && jwtTokenProvider.validateToken(token)) {
+//			// (추가) Redis 에 해당 accessToken logout 여부 확인
+//			String isLogout = redisTemplate.opsForValue().get(token);
+//			if (ObjectUtils.isEmpty(isLogout)) {
+//				// 토큰이 유효할 경우 토큰에서 Authentication 객체를 가지고 와서 SecurityContext 에 저장
+//				Authentication authentication = jwtTokenProvider.getAuthentication(token);
+//				SecurityContextHolder.getContext().setAuthentication(authentication);
+//			}
+//		}
 		chain.doFilter(request, response);
 	}
 
