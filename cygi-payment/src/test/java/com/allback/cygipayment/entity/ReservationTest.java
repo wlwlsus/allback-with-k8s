ddd127acafe733e93b3ebffdf0bf3c8761cf8743
성원준 <cadqe13@gmail.com>
2023-05-07 01:30:51 +0900

    fix: 테스트 코드 제거

--- cygi-payment/src/test/java/com/allback/cygipayment/entity/ReservationTest.java
+++ cygi-payment/src/test/java/com/allback/cygipayment/entity/ReservationTest.java
@@ -8,26 +8,26 @@ import org.springframework.test.annotation.Commit;
 
 @SpringBootTest
 public class ReservationTest {
-    @Autowired
-    ReservationRepository reservationRepository;
-
-    @Test
-    @Commit
-    public void addReservation() {
-        for (int i = 0; i < 100; i++) {
-            Reservation reservation = Reservation.builder()
-                .concertId(i%3)
-                .price(100)
-                .seat("A-" + i)
-                .build();
-            if(i%3==0) {
-                reservation.setStatus("예약중");
-            } else if(i%3==1) {
-                reservation.setStatus("예약완료");
-            } else {
-                reservation.setStatus("환불완료");
-            }
-            reservationRepository.save(reservation);
-        }
-    }
+//    @Autowired
+//    ReservationRepository reservationRepository;
+//
+//    @Test
+//    @Commit
+//    public void addReservation() {
+//        for (int i = 0; i < 100; i++) {
+//            Reservation reservation = Reservation.builder()
+//                .concertId(i%3)
+//                .price(100)
+//                .seat("A-" + i)
+//                .build();
+//            if(i%3==0) {
+//                reservation.setStatus("예약중");
+//            } else if(i%3==1) {
+//                reservation.setStatus("예약완료");
+//            } else {
+//                reservation.setStatus("환불완료");
+//            }
+//            reservationRepository.save(reservation);
+//        }
+//    }
 }
