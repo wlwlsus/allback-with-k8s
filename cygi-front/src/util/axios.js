8798b4e9212e0034687d8267d9fa1ee7f72c9f08
kjskjs356 <kjskjs356@gmail.com>
2023-05-08 14:18:26 +0900

    fix: url 수정

--- cygi-front/src/util/axios.js
+++ cygi-front/src/util/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const $_user = axios.create({
-  baseURL: "http://allback.site/api/v1",
+  baseURL: "http://allback.site:8081/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
