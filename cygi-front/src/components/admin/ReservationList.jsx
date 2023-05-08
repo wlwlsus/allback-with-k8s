4532f46a841a7283ea560dddecdfd0029463ecf4
성원준 <cadqe13@gmail.com>
2023-05-08 15:38:17 +0900

    upload: 프로젝트 추가

---
+++ cygi-front/src/components/admin/ReservationList.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import style from "./ReservationList.module.css";
+import { useQuery } from "@tanstack/react-query";
+import { $_admin } from "util/axios";
+
+export default function ReservationList() {
+  // 전체 예매내역 조회
+  const { isLoading, data } = useQuery(["reservation"], () =>
+    $_admin.get(`/dashboard/reservation`)
+  );
+
+  return (
+    <div className={style.container}>
+      <div className={style.reservation}>
+        <div className={style.title}>
+          <span>전체 예매 내역</span>
+        </div>
+        <div className={style.reservation_header}>
+          <div className={style.reserve_date}>예매 일자</div>
+          <div className={style.concert_name}>공연명</div>
+          <div className={style.reservationist}>예매자</div>
+          <div className={style.seat_num}>좌석 번호</div>
+          <div className={style.status}>상태</div>
+        </div>
+      </div>
+    </div>
+  );
+}
