4a2bb5f19f1ef658fb861f3238c8a2e1ad858dd9
kjskjs356 <kjskjs356@gmail.com>
2023-05-11 17:06:09 +0900

    fix: api url 수정

--- cygi-front/src/util/axios.js
+++ cygi-front/src/util/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const $ = axios.create({
-  baseURL: "http://k8a806.p.ssafy.io:8090/api/v1",
+  baseURL: "http://allback.site:8080/",
+  // baseURL: "http://localhost:8080/",
   headers: {
     "Content-Type": "application/json; charset=UTF-8",
   },
