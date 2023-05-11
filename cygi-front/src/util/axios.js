ad418a45196ac9ce214defb7b88cd9e1099012c9
kjskjs356 <kjskjs356@gmail.com>
2023-05-11 17:00:47 +0900

    fix: 마이페이지 API 오류 수정

--- cygi-front/src/util/axios.js
+++ cygi-front/src/util/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const $ = axios.create({
-  baseURL: "http://allback.site:8080/",
+  baseURL: "http://k8a806.p.ssafy.io:8090/api/v1",
   headers: {
     "Content-Type": "application/json; charset=UTF-8",
   },
