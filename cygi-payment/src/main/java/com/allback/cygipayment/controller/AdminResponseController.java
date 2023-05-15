98a0319eccda8be56b2e8c55978b5e4bf92665de
hosanyoon <yoonhosan@naver.com>
2023-05-15 10:19:44 +0900

    feat : 정산내역조회

--- cygi-payment/src/main/java/com/allback/cygipayment/controller/AdminResponseController.java
+++ cygi-payment/src/main/java/com/allback/cygipayment/controller/AdminResponseController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,34 +23,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AdminResponseController {
 
-  private final ReservationService reservationService;
-  private final BalanceServiceImpl balanceService;
+    private final ReservationService reservationService;
+    private final BalanceServiceImpl balanceService;
 
-  @GetMapping("/reservations")
-  ResponseEntity<ReservationListResAllDto> getReservations(
-      @Schema(hidden = true)
-      Pageable pageable,
-      @Schema(description = "페이지 별 개수", example = "10")
-      @RequestParam
-      int size,
-      @Schema(description = "페이지", example = "0")
-      @RequestParam
-      int page
-  ) {
-    System.out.println("reservations 진입");
-    ReservationListResAllDto resPage = reservationService.getAllReservations(pageable);
-    return ResponseEntity.status(HttpStatus.OK).body(resPage);
-  }
-  @GetMapping("/balances")
-  ResponseEntity<Page<BalanceStateResDto>> getBalances(
-      @Schema(description = "페이지", example = "0")
-      @RequestParam
-      int page,
-      @Schema(description = "페이지 별 개수", example = "10")
-      @RequestParam
-      int size
-  ) {
-      Page<BalanceStateResDto> balanceState = balanceService.getBalances(page, size);
-      return new ResponseEntity<>(balanceState, HttpStatus.OK);
-  }
+    @GetMapping("/reservations")
+    ResponseEntity<ReservationListResAllDto> getReservations(
+        @Schema(hidden = true)
+        Pageable pageable,
+        @Schema(description = "페이지 별 개수", example = "10")
+        @RequestParam
+        int size,
+        @Schema(description = "페이지", example = "0")
+        @RequestParam
+        int page
+    ) {
+        System.out.println("reservations 진입");
+        ReservationListResAllDto resPage = reservationService.getAllReservations(pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(resPage);
+    }
+
+    @GetMapping("/balances")
+    ResponseEntity<Page<BalanceStateResDto>> getBalances(
+        @Schema(description = "페이지", example = "0")
+        @RequestParam
+        int page,
+        @Schema(description = "페이지 별 개수", example = "10")
+        @RequestParam
+        int size,
+        @RequestHeader("Authorization") String authorization
+    ) {
+        Page<BalanceStateResDto> balanceState = balanceService.getBalances(page, size);
+        return new ResponseEntity<>(balanceState, HttpStatus.OK);
+    }
 }
