e712312f188520694f7b348188b78c5b4d2c921a
성원준 <cadqe13@gmail.com>
2023-05-17 10:01:12 +0900

    fix: user 유레카 삭제

--- cygi-user/src/main/java/com/allback/cygiuser/CygiUserApplication.java
+++ cygi-user/src/main/java/com/allback/cygiuser/CygiUserApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class CygiUserApplication {
 
